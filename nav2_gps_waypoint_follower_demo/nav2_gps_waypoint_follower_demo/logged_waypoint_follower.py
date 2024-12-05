d904bd80f768db227df7dd14de59fd15673196fa
luispri2001 <lpriel00@estudiantes.unileon.es>
2024-12-05 13:51:18 +0100

    fix mistake in file content

--- nav2_gps_waypoint_follower_demo/nav2_gps_waypoint_follower_demo/logged_waypoint_follower.py
+++ nav2_gps_waypoint_follower_demo/nav2_gps_waypoint_follower_demo/logged_waypoint_follower.py
@@ -1,79 +1,107 @@
 import rclpy
-from rclpy.node import Node
 from nav2_simple_commander.robot_navigator import BasicNavigator
-from geometry_msgs.msg import PointStamped
+import yaml
+from ament_index_python.packages import get_package_share_directory
+import os
+import sys
+import time
+from robot_localization.srv import FromLL
+from rclpy.node import Node
 from nav2_gps_waypoint_follower_demo.utils.gps_utils import latLonYaw2Geopose
 from nav2_msgs.action import FollowWaypoints
 from geometry_msgs.msg import PoseStamped
-from robot_localization.srv import FromLL
 
 
-class InteractiveGpsWpCommander(Node):
+class YamlWaypointParser:
     """
-    ROS2 node to send gps waypoints to nav2 received from mapviz's point click publisher
+    Parse a set of gps waypoints from a yaml file
     """
 
-    def __init__(self):
-        super().__init__(node_name="gps_wp_commander")
+    def __init__(self, wps_file_path: str) -> None:
+        with open(wps_file_path, "r") as wps_file:
+            self.wps_dict = yaml.safe_load(wps_file)
+
+    def get_wps(self):
+        """
+        Get an array of geographic_msgs/msg/GeoPose objects from the yaml file
+        """
+        gepose_wps = []
+        for wp in self.wps_dict["waypoints"]:
+            latitude, longitude, yaw = wp["latitude"], wp["longitude"], wp["yaw"]
+            gepose_wps.append(latLonYaw2Geopose(latitude, longitude, yaw))
+        return gepose_wps
+
+
+class GpsWpCommander(Node):
+    """
+    Class to use nav2 gps waypoint follower to follow a set of waypoints logged in a yaml file
+    """
+
+    def __init__(self, wps_file_path):
+        super().__init__("minimal_client_async")
         self.navigator = BasicNavigator("basic_navigator")
-        self.mapviz_wp_sub = self.create_subscription(
-            PointStamped, "/clicked_point", self.mapviz_wp_cb, 1
-        )
-
+        self.wp_parser = YamlWaypointParser(wps_file_path)
         self.localizer = self.create_client(FromLL, "/fromLL")
         while not self.localizer.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info("Service not available, waiting again...")
-        self.client_futures = []
+            self.get_logger().info("service not available, waiting again...")
 
-        self.get_logger().info("Ready for waypoints...")
-
-    def mapviz_wp_cb(self, msg: PointStamped):
+    def start_wpf(self):
         """
-        clicked point callback, sends received point to nav2 gps waypoint follower if its a geographic point
+        Function to start the waypoint following
         """
-        if msg.header.frame_id != "wgs84":
-            self.get_logger().warning(
-                "Received point from mapviz that ist not in wgs84 frame. This is not a gps point and wont be followed"
-            )
-            return
+        self.navigator.waitUntilNav2Active(localizer="controller_server")
+        wps = self.wp_parser.get_wps()
 
-        wps = [latLonYaw2Geopose(msg.point.y, msg.point.x)]
-
+        wpl = []
         for wp in wps:
             self.req = FromLL.Request()
             self.req.ll_point.longitude = wp.position.longitude
             self.req.ll_point.latitude = wp.position.latitude
             self.req.ll_point.altitude = wp.position.altitude
 
-            self.get_logger().info("Waypoint added to conversion queue...")
-            self.client_futures.append(self.localizer.call_async(self.req))
+            log = "long{:f}, lat={:f}, alt={:f}".format(
+                self.req.ll_point.longitude,
+                self.req.ll_point.latitude,
+                self.req.ll_point.altitude,
+            )
+            self.get_logger().info(log)
 
-    def command_send_cb(self, future):
-        self.resp = PoseStamped()
-        self.resp.header.frame_id = "map"
-        self.resp.header.stamp = self.get_clock().now().to_msg()
-        self.resp.pose.position = future.result().map_point
+            self.future = self.localizer.call_async(self.req)
+            rclpy.spin_until_future_complete(self, self.future)
 
-        self.navigator.goToPose(self.resp)
+            self.resp = PoseStamped()
+            self.resp.header.frame_id = "map"
+            self.resp.header.stamp = self.get_clock().now().to_msg()
+            self.resp.pose.position = self.future.result().map_point
 
-    def spin(self):
-        while rclpy.ok():
-            rclpy.spin_once(self)
-            incomplete_futures = []
-            for f in self.client_futures:
-                if f.done():
-                    self.get_logger().info("Following converted waypoint...")
-                    self.command_send_cb(f)
-                else:
-                    incomplete_futures.append(f)
+            log = "x={:f}, y={:f}, z={:f}".format(
+                self.future.result().map_point.x,
+                self.future.result().map_point.y,
+                self.future.result().map_point.z,
+            )
+            self.get_logger().info(log)
 
-            self.client_futures = incomplete_futures
+            self.resp.pose.orientation = wp.orientation
+            wpl += [self.resp]
+
+        self.navigator.followWaypoints(wpl)
+        print("wps completed successfully")
 
 
 def main():
     rclpy.init()
-    gps_wpf = InteractiveGpsWpCommander()
-    gps_wpf.spin()
+
+    # allow to pass the waypoints file as an argument
+    default_yaml_file_path = os.path.join(
+        get_package_share_directory("node"), "config", "demo_waypoints.yaml"
+    )
+    if len(sys.argv) > 1:
+        yaml_file_path = sys.argv[1]
+    else:
+        yaml_file_path = default_yaml_file_path
+
+    gps_wpf = GpsWpCommander(yaml_file_path)
+    gps_wpf.start_wpf()
 
 
 if __name__ == "__main__":
